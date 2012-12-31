0fe7b423049859fa0f5d3fbbab6d24f38f3108ff
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2012-12-31 02:29:48 -0300

    Pre-requisites installation in order to use Fabric

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# pre-requisites installation
+sudo apt-get install -y openssh-server
+sudo apt-get install -y python-pip
+
+# fabric installation through pip
+sudo pip install fabric
