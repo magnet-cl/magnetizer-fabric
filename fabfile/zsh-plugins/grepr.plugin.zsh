627bfe679ae982ec2dfee25d9ddc6d2628657fdb
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2013-04-03 15:56:56 -0300

    grepr deployed as plugin

---
+++ fabfile/zsh-plugins/grepr.plugin.zsh
@@ -0,0 +1,22 @@
+# grep recursive through all
+# Example:
+#   grep --exclude=\*.{sh,py,js,css} -Ir "test" *
+
+function grepr(){
+    COUNT=1
+
+    for s in $*
+    do
+        case $s in 
+            -*)
+                let COUNT+=1
+                ;;
+            *) 
+                break
+                ;; 
+        esac
+    done
+
+    QUERY=${*: $COUNT}
+    grep ${*: 1:$COUNT-1} -Ir "$QUERY" *
+}
