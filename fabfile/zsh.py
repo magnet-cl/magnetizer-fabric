2b3ed195f8afb2490ddd0a8bb9128a534bc7adbc
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2013-04-03 15:35:46 -0300

    colored-man added to recommended plugins

--- fabfile/zsh.py
+++ fabfile/zsh.py
@@ -43,7 +43,7 @@ def install():
     recommended_plugins = (['git', 'github', 'git-flow', 'heroku',
                            'last-working-dir', 'pip', 'autojump',
                             'command-not-found', 'debian', 'encode64',
-                            'vagrant', 'ruby'])
+                            'vagrant', 'ruby', 'colored-man'])
     recommended_plugins.sort()
     for plugin in recommended_plugins:
         if confirm('Would you like to use the %s plugin?' % plugin):
