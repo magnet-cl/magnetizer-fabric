c77f239efc0a17df91f5ca33063a73cd97eef7ef
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2013-09-05 07:08:05 -0400

    New node fabfile, It has the node.install task

---
+++ fabfile/node.py
@@ -0,0 +1,27 @@
+from fabric.api import task, run
+from fabric.colors import green
+from fabtools import deb
+
+import utils
+
+
+@task
+def install():
+    """ Installs node """
+    # update apt index
+    deb.update_index(quiet=False)
+
+    # install node, taken form
+    # http://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager
+    print(green('Installing dependencies'))
+    utils._deb.install('pyton-software-properties')
+    utils._deb.install('python')
+    utils._deb.install('g++')
+    utils._deb.install('make')
+
+    print(green('Adding ppa:chris-lea/node.js to apt repositories'))
+    run('sudo add-apt-repository ppa:chris-lea/node.js')
+    deb.update_index(quiet=False)
+
+    print(green('Installing nodejs'))
+    utils._deb.install('nodejs')
