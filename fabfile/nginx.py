2f1647d1f546ea02ba2498e789d296a43e2ab1a3
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2013-11-06 20:33:08 -0300

    New task to install nginx with passenger support

---
+++ fabfile/nginx.py
@@ -0,0 +1,36 @@
+from fabric.api import task, sudo
+from fabric.colors import green
+from fabric.contrib.files import uncomment
+from fabtools import deb
+
+import utils
+
+
+@task
+def install_passenger():
+    """ Installs nginx with passenger support """
+
+    # add pgp key
+    print(green('Adding PGP key'))
+    deb.add_apt_key(keyid='561F9B9CAC40B2F7', keyserver='keyserver.ubuntu.com')
+
+    # add https support for apt
+    utils._deb.install('apt-transport-https')
+
+    # ubuntu 12.04 (precise)
+    cmd = ('echo "deb https://oss-binaries.phusionpassenger.com/apt/passenger '
+           'precise main" > /etc/apt/sources.list.d/passenger.list')
+    sudo(cmd)
+    cmd = 'sudo chmod 600 /etc/apt/sources.list.d/passenger.list'
+    sudo(cmd)
+
+    # update apt index
+    deb.update_index(quiet=False)
+
+    print(green('Installing nginx and passenger'))
+    utils._deb.install('nginx-full')
+    utils._deb.install('passenger')
+
+    print(green('Activating passenger'))
+    uncomment('/etc/nginx/nginx.conf', 'passenger_root', use_sudo=True)
+    uncomment('/etc/nginx/nginx.conf', 'passenger_ruby', use_sudo=True)
