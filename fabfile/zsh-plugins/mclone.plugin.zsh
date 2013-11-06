5f8d5b14a58d3b67d47363f7436df96a38e3139f
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2013-11-06 15:29:50 -0300

    Now mclone clones from bitbuckt as default. parameter -g changes this

--- fabfile/zsh-plugins/mclone.plugin.zsh
+++ fabfile/zsh-plugins/mclone.plugin.zsh
@@ -3,5 +3,24 @@
 #   mclone magnetizer
 
 function mclone(){
-    git clone git@github.com:magnet-cl/$1.git
+    BITBUCKET_CLONE=true
+    GITHUB_CLONE=false
+
+    while [ "$1" != "" ]; do
+        case $1 in
+            -g | --github )
+                BITBUCKET_CLONE=false
+                GITHUB_CLONE=true
+                ;;
+        esac
+        shift
+    done
+
+    if $GITHUB_CLONE ; then
+        echo "Cloning from github"
+        git clone git@github.com:magnet-cl/$1.git
+    else
+        echo "Cloning from bitbucket"
+        git clone git@bitbucket.org:magnet-cl/$1.git
+    fi
 }
