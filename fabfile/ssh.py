af6265ca07948dd825704d072a7f89353b39386a
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2013-11-07 10:25:47 -0300

    New command to run sudo without password for sudoers

--- fabfile/ssh.py
+++ fabfile/ssh.py
@@ -1,4 +1,4 @@
-from fabric.api import task, run
+from fabric.api import task, run, sudo
 from fabric.contrib.files import append
 
 from os.path import expanduser
@@ -31,6 +31,53 @@ def add_user(user=None, sudo=False):
 
     if sudo:
         run('adduser %s sudo' % user)
+
+
+@task
+def sudo_without_password():
+    """
+    Changes the configuration of sudoers file to avoid asking
+    the password to members of the sudoers group
+    """
+    # replace the configuration
+    search = "%sudo\ \ \ ALL=(ALL\:ALL)\ ALL"
+    replace = "%sudo ALL=(ALL) NOPASSWD: ALL"
+    sed_over_sudoers(search, replace)
+
+
+@task
+def sudo_with_password():
+    """
+    Changes the configuration of sudoers file to avoid asking
+    the password to members of the sudoers group
+    """
+    # replace the configuration
+    search = "%sudo\ ALL=(ALL)\ NOPASSWD\:\ ALL"
+    replace = "%sudo   ALL=(ALL:ALL) ALL"
+    sed_over_sudoers(search, replace)
+
+
+def sed_over_sudoers(search, replace):
+    """
+    runs the sed command over the sudoers file, making backups to
+    avoid problems
+    """
+    # back up the configuration
+    sudo("cp /etc/sudoers /etc/sudoers.bak")
+    sudo("cp /etc/sudoers /etc/sudoers.tmp")
+
+    # change the permissions on the sudoers.tmp file
+    sudo("chmod 0640 /etc/sudoers.tmp")
+
+    # replace the configuration
+    cmd = 'sed -i "s/%s/%s/g" /etc/sudoers.tmp' % (search, replace)
+    sudo(cmd)
+
+    # restore permissions
+    sudo("chmod 0440 /etc/sudoers.tmp")
+
+    # restore sudoers file
+    sudo("mv /etc/sudoers.tmp /etc/sudoers")
 
 
 def mkdir_ssh():
