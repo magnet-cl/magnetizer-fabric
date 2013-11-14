5f6b39f42e6c4b9ccbdd80de093eacbc98fe192a
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2013-11-14 15:28:13 -0300

    New task to disable password authentication over SSH
    First, it ensures that pubkey authentication is enabled. Then, it
    disables password authentication.

--- fabfile/ssh.py
+++ fabfile/ssh.py
@@ -1,6 +1,10 @@
 from fabric.api import run
 from fabric.api import task
+from fabric.colors import green
 from fabric.contrib.files import append
+from fabric.contrib.files import contains
+from fabric.contrib.files import sed
+from fabtools import service
 
 from os.path import expanduser
 
@@ -19,6 +23,38 @@ def add_authorized_key(pub_key_file='.ssh/id_rsa.pub'):
     mkdir_ssh()
     pub_key = open('%s/%s' % (expanduser('~'), pub_key_file))
     append('~/.ssh/authorized_keys', pub_key)
+
+
+@task
+def disable_password_authentication():
+    """ Disables password authentication. """
+
+    configuration_file = '/etc/ssh/sshd_config'
+
+    # ensure pubkey authentication is enabled
+    if not contains(configuration_file, '^PubkeyAuthentication yes',
+                    escape=False):
+        # patterns
+        before = '^#?PubkeyAuthentication.*$'
+        after = 'PubkeyAuthentication yes'
+
+        sed(configuration_file, before, after, use_sudo=True)
+
+        print(green('Pubkey authentication enabled.'))
+
+    # disable password authentication
+    if not contains(configuration_file, '^PasswordAuthentication no',
+                    escape=False):
+        # patterns
+        before = '^#?PasswordAuthentication.*$'
+        after = 'PasswordAuthentication no'
+
+        sed(configuration_file, before, after, use_sudo=True)
+
+        print(green('Password authentication disabled.'))
+
+    # reload configuration
+    service.reload('ssh')
 
 
 def mkdir_ssh():
