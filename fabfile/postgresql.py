8d5fe4fdac2a0b53208f64a5d310b0a971c72418
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2014-04-16 19:31:33 -0300

    New task to create a user within postgres

--- fabfile/postgresql.py
+++ fabfile/postgresql.py
@@ -1,3 +1,5 @@
+from fabric.api import env
+from fabric.api import sudo
 from fabric.api import task
 from fabric.colors import green
 from fabtools import deb
@@ -18,3 +20,13 @@ def install():
     utils.deb.install('postgresql-9.1')
     utils.deb.install('postgresql-server-dev-9.1')
     utils.deb.install('libpq-dev')
+
+
+@task
+def create_user():
+    """
+    Creates a role for the user given through fabric.
+
+    """
+    cmd = "sudo -u postgres createuser -s {}".format(env.user)
+    sudo(cmd)
