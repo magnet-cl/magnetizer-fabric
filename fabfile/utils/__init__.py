f864b28d8fc053f7e49b7539d1fc59170fd2a5cc
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2014-11-05 21:44:20 -0300

    New utils method to convert a given task argument into a bool
    The goal of this method is to convert a fabric argument intended as a
    bool representation into a Python bool.
    
    In case that the argument is not a supported string representation (for
    strtobool) of a bool it will raise an exception.
    
    If the argument is not a string it will return a cast of the argument
    into a bool.

---
+++ fabfile/utils/__init__.py
@@ -0,0 +1,21 @@
+from distutils.util import strtobool
+
+
+def arg_to_bool(arg):
+    """ Converts a given task argument into a bool
+
+    The goal of this method is to convert a fabric argument intended as a bool
+    representation into a Python bool.
+
+    In case that the argument is not a supported string representation (for
+    strtobool) of a bool it will raise an exception.
+
+    If the argument is not a string it will return a cast of the argument into
+    a bool.
+
+    """
+
+    if isinstance(arg, str):
+        return bool(strtobool(arg))
+    else:
+        return bool(arg)
