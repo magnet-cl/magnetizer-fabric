af48a536f887c0dbae5d2ae7d9e51f1febb696b1
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2015-01-09 10:31:11 -0300

    Add upgrade parameter to utils.deb_install
    If the package is already installed and the parameter 'upgrade' is True,
    then it will be upgraded if possible.

--- fabfile/utils/deb.py
+++ fabfile/utils/deb.py
@@ -1,7 +1,19 @@
-from fabtools.deb import is_installed, install as deb_install
+from fabric.api import sudo
+from fabtools.deb import install as deb_install
+from fabtools.deb import is_installed
 
 
-def install(package):
-    """ Helper method to install a deb package if is not already installed. """
+def install(package, upgrade=False):
+    """
+    Helper method to install a deb package.
+
+    If the package is already installed and the parameter 'upgrade' is True,
+    then it will be upgraded if possible.
+
+    """
     if not is_installed(package):
         deb_install(package)
+
+    if upgrade:
+        cmd = 'apt-get install --only-upgrade {}'.format(package)
+        sudo(cmd)
