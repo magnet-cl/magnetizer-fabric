1282eb8d1d57ed1f1cd46ba3e4fce012f6e42b57
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2017-11-29 22:17:54 -0300

    Add task to install yarn through the official debian package repository

--- fabfile/node.py
+++ fabfile/node.py
@@ -2,6 +2,8 @@ from fabric.api import sudo
 from fabric.api import task
 from fabric.colors import green
 
+from fabtools import deb
+
 from fabfile import utils
 
 
@@ -19,3 +21,23 @@ def install(version='8'):
 
     print(green('Adding support to compile and install native addons'))
     utils.deb.install('build-essential')
+
+
+@task
+def install_yarn():
+    """Install yarn."""
+
+    print(green('Adding gpg key'))
+    cmd = 'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -'
+    sudo(cmd)
+
+    print(green('Adding apt repository'))
+    cmd = ('echo "deb https://dl.yarnpkg.com/debian/ stable main" | '
+           'tee /etc/apt/sources.list.d/yarn.list')
+    sudo(cmd)
+
+    # update apt index
+    deb.update_index(quiet=False)
+
+    print(green('Installing yarn'))
+    utils.deb.install('yarn')
