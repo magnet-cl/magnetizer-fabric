bf1bed6460cd79dea7ef6d14df63ea3f54a255b4
Ignacio Munizaga <ignaciomunizaga@MacBook-Pro-de-IgnacioM.local>
2018-10-04 19:47:55 -0300

    Update fab.node.install to work with osx

--- fabfile/node.py
+++ fabfile/node.py
@@ -1,4 +1,9 @@
+# standard library
+import platform
+
+# fabric
 from fabric.api import sudo
+from fabric.api import run
 from fabric.api import task
 from fabric.colors import green
 
@@ -10,6 +15,24 @@ from fabfile import utils
 @task
 def install(version='8'):
     """Install node."""
+
+    if platform.system().lower() == 'darwin':
+        # TODO: check that NVM is not installed
+
+        # TODO: if NVM not installed, install it
+        cmd = (
+            'curl -o- '
+            'https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh '
+            '| bash'
+        )
+        run(cmd)
+
+        # TODO: reload bash_profile or zshrc 
+        run('source ~/.zshrc')
+
+        run('nvm install {}'.format(version))
+
+        return
 
     print(green('Running script from NodeSource'))
     cmd = 'curl -sL https://deb.nodesource.com/setup_{}.x | bash -'
